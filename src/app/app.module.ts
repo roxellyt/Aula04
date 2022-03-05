0ff866dd3f5f78eb2fcc4c9ee26d9e2b80c893e2
roxellyt <roxelly.tavares@gmail.com>
2022-03-04 21:15:12 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { HelloComponent } from './hello.component';
-import { TabuadaComponent } from './tabuada.component';
-import { AboutComponent } from './about.component';
+import { TabuadaComponent } from './tabuada/tabuada.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'tabuada', component: TabuadaComponent },
+      { path: 'about', component: AboutComponent },
+    ]),
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
+    HomeComponent,
     TabuadaComponent,
     AboutComponent,
   ],
